7fef5c31d767d8b0e8339aeb125694d77c735345
chiu <corinna930512@gmail.com>
2025-06-16 15:58:04 +0800

    改驗證小東西

--- firebase_config.py
+++ firebase_config.py
@@ -1,24 +1,18 @@
 import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
+import json
+#可以優化成相對路徑之後弄
+with open("C:/Users/Chiu ling/Desktop/12projectback/study_robot_backend/firebase/firebase_config_dict.json") as f:
+    firebase_config_dict = json.load(f)
+
 
 # Firebase 初始化函數
 def initialize_firebase():
     if not firebase_admin._apps:  # 確保只初始化一次
 
         cred = credentials.Certificate('C:/Users/Chiu ling/Desktop/12projectback/study_robot_backend/firebase/serviceAccountKey.json')
-        firebase_admin.initialize_app(cred,{'storageBucket':'project-771d3.firebasestorage.app'})
-
-firebase_config_dict = {
-    "apiKey": "AIzaSyCGjCb6y-3_gvAGPJVDVmDM4EmZ226VuNA",
-    "authDomain": "project-771d3.firebaseapp.com",
-    "databaseURL": "https://project-771d3-default-rtdb.firebaseio.com",
-    "projectId": "project-771d3",
-    "storageBucket": "project-771d3.appspot.com",
-    "messagingSenderId": "745736050770",
-    "appId": "1:745736050770:web:7b926b93375c6e59f2678b",
-    "measurementId": "G-5PPK6CZFSC"
-}
+        firebase_admin.initialize_app(cred,{'storageBucket': 'project-771d3.appspot.com'})
 
 
 # 確保 Firebase 已初始化
